30598ec2e47001d0415eddbc83ec728ba4bd0502
Moh-it1 <165020104+Moh-it1@users.noreply.github.com>
2026-05-13 12:14:06 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Typing Effect
+
+const text = [
+  "Web Developer",
+  "Programmer",
+  "Student"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type(){
+
+  if(count === text.length){
+    count = 0;
+  }
+
+  currentText = text[count];
+
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typing").textContent = letter;
+
+  if(letter.length === currentText.length){
+
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 200);
+}
+
+type();
+
+
+// Contact Button
+
+function showMessage(){
+
+  alert("Message Sent Successfully!");
+}
